d597d03534f9dd8e37f8a6398c33947c6ed90cdb
Orinbayev <amirxonorinbayev5854@gmail.com>
2024-05-09 22:06:33 +0300

    Kalkulyator

---
+++ call/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculator(request):
+	if request.method == 'POST':
+		result = request.POST.get('result', '')
+
+		return render(request, 'index.html', {'result': result})
+
+	return render(request, 'index.html')
